4e9d068e9e896a0727520d0c9a7f017fb1752a56
lysmik <lysmik@rarus.ru>
2017-09-04 13:30:45 +0300

    Project structure

--- app/src/main/java/com/miky/dev/dribbbleapp/ui/MainActivity.java
+++ app/src/main/java/com/miky/dev/dribbbleapp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.miky.dev.dribbbleapp;
+package com.miky.dev.dribbbleapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.miky.dev.dribbbleapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
