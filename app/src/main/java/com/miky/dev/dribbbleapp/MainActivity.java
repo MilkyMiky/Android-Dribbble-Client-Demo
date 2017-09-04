7c3e251d7b6e30f4c2f525df50c01bf5cbbe0256
lysmik <lysmik@rarus.ru>
2017-09-04 13:22:25 +0300

    Init commit

---
+++ app/src/main/java/com/miky/dev/dribbbleapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.miky.dev.dribbbleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
